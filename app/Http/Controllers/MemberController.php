39dacb95cdcaa99c01978d49ed62f1e88c156b65
infomotin <motin.mmk.jr@gmail.com>
2025-03-11 14:10:50 +0600

    logout

---
+++ app/Http/Controllers/MemberController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+
+class MemberController extends Controller
+{
+    //logout
+
+    public function MemberLogout(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\MemberController;
 use App\Models\Admin;
 
 Route::get('/', function () {
@@ -13,6 +14,11 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('member.member_dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+//
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/logout', [MemberController::class, 'MemberLogout'])->name('member.logout');
+});
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
